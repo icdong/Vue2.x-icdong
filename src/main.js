1012cb41da8169611496c17dcdf447d3d7f590a3
icdong <i@icdong.com>
2020-09-05 23:02:52 +0800

    UI配置

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 import http from './libs/http'
 Vue.prototype.$http = http
 
+// UI配置
 Vue.use(ElementUI)
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
 Vue.config.productionTip = false;
 
 new Vue({
